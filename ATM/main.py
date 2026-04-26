aec87154a911241726654bb03b47c3c799a9c7a3
h2501001184 <2501001184@cgcjhanjeri.in>
2026-04-27 00:13:10 +0530

    Update main.py

--- ATM/main.py
+++ ATM/main.py
@@ -5,11 +5,13 @@ def show_menu():
     print("1. Check Balance")
     print("2. Deposit")
     print("3. Withdraw")
-    print("4. Exit")
+    print("4. Mini Statement")
+    print("5. Exit")
 
 
 def run_atm():
     print("Welcome to Our ATM Service!")
+    transaction_history=[]
 
     while True:
         show_menu()
@@ -22,6 +24,7 @@ def run_atm():
             amt=int(input("Enter the amount to Deposite: "))
             if transaction.deposit(amt):
                 print(f"{amt} is Deposited Succesfully. Your current Balance is {transaction.check_balance()}") 
+                transaction_history.append(f"{amt} deposit")
             else :
                 print("Earn Some Money! You Looser!")
 
@@ -29,10 +32,20 @@ def run_atm():
             amt=int(input("Enter the amount to Withdraw: "))
             if transaction.withdraw(amt):
                 print(f"Withdraw succesful {amt} is in your hand. Use it wisely.")
+                transaction_history.append(f"{amt} withdraw")
             else:
                 print("Earn some money to withdraw from you account! NOOB!")
-
+        
         elif choice == 4:
+            print("\n--- YOUR MINI STATEMENT ---")
+            if not transaction_history:
+                print("No transactions recorded yet.")
+            else:
+                for entry in transaction_history:
+                    print(f" - {entry}")
+            print("---------------------------")
+
+        elif choice == 5:
             print("Thank you for using the ATM. Goodbye!")
             break
 
